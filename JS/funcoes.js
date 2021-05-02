ba878d51a1fe99df32fa73d8029d0e0abc1d81b6
alex-arantes <projetodigital@gmail.com>
2021-05-02 16:49:13 -0300

    Salvar

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,14 +1,4 @@
-//-----------------------------------------------------------------------------------------------------------
-// Função: validarProduto(idNomeProduto, idCodProduto, idQtidadeProduto)
-// Verifica se foram informados o nome e o código do produto
-// Parâmetros:
-// - idNomeProduto: id do campo que contém o nome do produto
-// - idCodProduto: id do campo que contém o código do produto
-// - idQtidadeProduto: id do campo que contém a quantidade do produto
-// OBS: Se faltar alguma informação (nome ou código do produto) aparecerá uma mensagem de erro. Em caso de 
-// sucesso (todas as informações preenchidas), chama a função cadastrarProduto(...)
-// Retorno: nenhum
-//-----------------------------------------------------------------------------------------------------------
+
 function validarProduto(idNomeProduto, idCodProduto, idQtidadeProduto) {
     let nome = document.getElementById(idNomeProduto).value;
     let codigo = document.getElementById(idCodProduto).value;
@@ -20,17 +10,6 @@ function validarProduto(idNomeProduto, idCodProduto, idQtidadeProduto) {
         alert("Código do produto não pode estar em branco. Favor preenchê-lo!");
     else cadastrarProduto(nome, codigo, parseInt(qtidade));
 }
-//-----------------------------------------------------------------------------------------------------------
-// Função: cadastrarProduto(produto, codig, qtidade)
-// Cadastra um novo produto (nome, código e quantidade) no estoque
-// Parâmetros:
-// - produto: nome do produto a ser cadastrado no estoque (Ex: arroz)
-// - codig: código do produto a ser cadastrado no estoque (Ex: a01)
-// - qtidade: quantidade do produto a ser cadastrado no estoque (Ex: 7)
-// OBS: Apos cadastrar o novo produto no estoque, atualiza a quantidade de itens no carrinho, ou seja, chama 
-// a função atualizarTotalEstoque()
-// Retorno: nenhum
-//-----------------------------------------------------------------------------------------------------------
 function cadastrarProduto(produto, codig, qtidade) {
     let novoProduto = {nome:produto, codigo:codig, quantidade:qtidade};
 
@@ -47,23 +26,10 @@ function cadastrarProduto(produto, codig, qtidade) {
     else alert("A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação");
 }
 
-//-----------------------------------------------------------------------------------------------------------
-// Função: atualizarTotalEstoque(idCampo)
-// Incrementa a quantidade de itens cadastrado no estoque 
-// Parâmetros:
-// - idCampo: identificador do campo que contem a quantidade de itens no estoque
-// Retorno: nenhum
-//-----------------------------------------------------------------------------------------------------------
+
 function atualizarTotalEstoque(idCampo) {
     localStorage.setItem("totalEstoque",++document.getElementById(idCampo).innerHTML)
 }
-//-----------------------------------------------------------------------------------------------------------
-// Função: carregarTotalEstoque(idCampo)
-// Incrementa a quantidade de itens cadastrado no estoque 
-// Parâmetros:
-// - idCampo: identificador do campo que contem a quantidade de itens no estoque
-// Retorno: nenhum
-//-----------------------------------------------------------------------------------------------------------
 function carregarTotalEstoque(idCampo) {
     if (typeof(Storage) !== "undefined") {
         let totalEstoque = localStorage.getItem("totalEstoque");
@@ -73,14 +39,10 @@ function carregarTotalEstoque(idCampo) {
     else alert("A versão do seu navegador é muito antiga. Por isso, não será possível executar essa aplicação");
 }
 
-//-----------------------------------------------------------------------------------------------------------
-// Exibe todos os itens do estoque (nome, código e quantidade)
-// Retorno: nenhum
-//-----------------------------------------------------------------------------------------------------------
 function listarEstoque() {
     if (typeof(Storage) !== "undefined") {
         let produtos = localStorage.getItem("produtos");
-        document.write("<h1>Estoque:</h1>")
+        document.write("<h1>Lista de Medicamentos:</h1>")
         if (produtos == null)
             document.write("<h3>Ainda não há nenhum item no estoque</h3>");
         else {
@@ -89,7 +51,7 @@ function listarEstoque() {
                 document.write("<ul>");
                 document.write("<li>Nome do produto: "+produto.nome+"</li>");
                 document.write("<li>Código do produto: "+produto.codigo+"</li>");
-                document.write("<li>Quantidade no estoque: "+produto.quantidade+"</li>");
+                document.write("<li>Quantidade: "+produto.quantidade+"</li>");
                 document.write("</ul>");
             });
         }
